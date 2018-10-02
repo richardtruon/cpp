86828487b56e807f7145b48396f78a504e7616c9
Rich T <a-dat@live.com>
2018-10-01 22:41:08 -0600

    Add files via upload

---
+++ 453/A1/Debug/glad.d
@@ -0,0 +1,7 @@
+glad.o: ../glad.c \
+ /home/ugb/eric.dimond/cpsc_453/A1/middleware/glad/include/glad/glad.h \
+ /home/ugb/eric.dimond/cpsc_453/A1/middleware/glad/include/KHR/khrplatform.h
+
+/home/ugb/eric.dimond/cpsc_453/A1/middleware/glad/include/glad/glad.h:
+
+/home/ugb/eric.dimond/cpsc_453/A1/middleware/glad/include/KHR/khrplatform.h:
